0a7c879ef19c6fc655da43fd673bbe2fd55124e9
Crist0796 <cjimenez@tl300.com.uy>
2023-11-20 06:48:29 -0300

    Agrego diversos cambios en la parte visual y agrego proyectos en el portafolio

--- js/app.js
+++ js/app.js
@@ -11,9 +11,35 @@ let words = [ //Array con las palabras que estarán animadas.
     "{'EMPRENDEDOR'}"]
 let charValidate="printcousg()'<>/;{}"//letras que serán de otro color
 
+
 let dWords = document.getElementById("logo-words")
 let cont = 0
 let long = 0
+
+const iconsClassToprojects = {
+    "LARAVEL" : "fab fa-laravel c-laravel",
+    "PHP" : "fab fa-php c-php",
+    "HTML" : "fab fa-html5 c-html",
+    "CSS" : "fab fab fa-css3-alt c-css",
+    "JAVA" : "fab fa-java c-java",
+    "KOTLIN" : "fas fa-file-code c-java",
+    "ANDROID" : "fab fa-android c-android",
+    "JAVASCRIPT" : "fab fa-js-square c-js",
+    "C++" : "fas fa-file-code c-java",
+    "PYTHON" : "fab fa-python c-python",
+    "REACT" : "fab fa-react c-react",
+}
+
+//-------Lo muestra las diferentes páginas en la sección de content---------------//
+let projects = document.getElementById("projects")
+let aboutMe = document.getElementById("about-me")
+let aboutMeBoton = document.getElementById("about-me-boton")
+let projectsBoton = document.getElementById("projects-boton")
+let projectsContentButtons = document.getElementById("projects-content-buttons")
+let projectsContent = document.getElementById("projects-content")
+let contactBoton = document.getElementById("contact-boton")
+let contact = document.getElementById("contact")
+let arrayProjects = [] // Proyectos a mostrar
 
 function haveChar(char){//Recibe como parametro una letra y será comparada con "charValidate" si son las mismas devuelve 1 (true)
     for(i=0; i<char.length; i++){
@@ -72,44 +98,107 @@ function writeWords(){
 }
 
 
+// Muestra los iconos de las tecnologías de los proyectos
+function getIcons(listItems){
+
+
+    let icons = listItems.split(',')
+    console.log(icons)
+    let iconElemnts = ""
+    let auxIconElemnts = ""
+
+    icons.forEach(icon => {
+        let classIcon = iconsClassToprojects[icon]
+        auxIconElemnts += `<i class="${classIcon}"></i>`
+    });
+
+    iconElemnts = `<div class="icons-projects-container">${auxIconElemnts}</div>`
+    return iconElemnts
+
+
+}
+
+
+// Carga las cards de los proyectos
+function getCardProjects(projects){
+    let content = ""
+    for(i in projects){
+        content += 
+        `<div class="card-projects m-1">                
+            <div class="card-projects-info">
+            <p class="card-projects-title">${projects[i].nombre}</p>
+                <div class="card-projects-body">
+                    <div class="card-projects-img">
+                        <img src="assets/img/projects/${projects[i].img}" alt="100" width="400">
+                    </div>
+                    <div class="card-projects-text">
+                        <P><span class="value c-primary"><strong>${projects[i].descriptionShort}</strong></span></P>
+                    </div>
+                    ${getIcons(projects[i].dev)}    
+                    <div class="card-projects-description">
+                        <p><strong>${projects[i].descriptionLong}.</strong></p>
+                        <div class="visit-button-container">
+                            <a href="${projects[i].link}" target="_blank">Visitar <i class="fas fa-eye"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
+    }
+    return content
+}
+
+function getButtonsProjectsContent(nButtons){
+    let buttons = ""
+    for(let i = 0; i < nButtons; i++){
+        buttons += `<p class="projects-content-button" onClick="getArrayProjects(${i})">${i+1}</p>`
+    }
+    return buttons
+}
+
+function getArrayProjects(i){
+    let projectsToShow = arrayProjects.slice(i*3, i*3+3)  
+    let auxContent = getCardProjects(projectsToShow)
+    projectsContent.innerHTML = auxContent
+}
+
+
 function showContent(){
     
 
     //--------------------Sidebar--------------------------------//
-    let botonSideRight = document.getElementById("side-icon-right")
-    let botonSideLeft = document.getElementById("side-icon-left")
+    let buttonSide = document.getElementById('icon-side')
     let textSide = document.getElementById("side-title-text")
     let skills = document.getElementById("skills")
     let qualities = document.getElementById("qualities")
-    let qualitiesAux = 0
-    let contSide = 1
+    let contSide = 0
+    let rotation = 0
 
     //botones del side bar para mostrar las habilidades y cualidades
-    botonSideRight.addEventListener("click", ()  => {
-            textSide.innerHTML = "CUALIDADES"
-            skills.style.opacity = "0"
-            window.setTimeout(() => {
-                skills.style.display = "none"
-                qualities.style.display = "flex"
-            }, 800)
-            qualities.style.opacity = "1"
-            botonSideLeft.style.display = "block";
-            botonSideRight.style.display = "none";
-            contSide = 0
-    })
-
-    botonSideLeft.addEventListener("click", () => {   
-        botonSideRight.style.display = "block";
-        textSide.innerHTML = "HABILIDADES"
-        qualities.style.opacity = "0"
-        window.setTimeout(() => {
-            skills.style.display = "flex"
-            qualities.style.display = "none"
-        }, 800)
-        skills.style.opacity = "1";
-        botonSideLeft.style.display = "none";
-        contSide ++
-      
+    buttonSide.addEventListener("click", ()  => {
+        let botonSideRight = document.getElementById("side-icon-right")
+        rotation += 180
+        botonSideRight.style.transform = `rotate(${rotation}deg)`
+            if(contSide === 0){
+                textSide.innerHTML = "CUALIDADES"
+                skills.style.opacity = "0"
+                window.setTimeout(() => {
+                    skills.style.display = "none"
+                    qualities.style.display = "flex"
+                }, 500)
+                qualities.style.opacity = "1"
+                contSide = 1
+            }else{
+                textSide.innerHTML = "HABILIDADES"
+                qualities.style.opacity = "0"
+                window.setTimeout(() => {
+                    skills.style.display = "flex"
+                    qualities.style.display = "none"
+                }, 500)
+                skills.style.opacity = "1";
+                contSide = 0
+            }
+            
     })
     //-----------------------------------------------------------//
 
@@ -132,14 +221,7 @@ function showContent(){
     })
 
     
-//-------Lo muestra las diferentes páginas en la sección de content---------------//
-    let projects = document.getElementById("projects")
-    let aboutMe = document.getElementById("about-me")
-    let aboutMeBoton = document.getElementById("about-me-boton")
-    let projectsBoton = document.getElementById("projects-boton")
-    let projectsContent = document.getElementById("projects-content")
-    let contactBoton = document.getElementById("contact-boton")
-    let contact = document.getElementById("contact")
+
 
     aboutMe.style.display = "flex" //Es el contenido que al cargar la página es visible
     contact.style.display = "none"
@@ -154,38 +236,22 @@ function showContent(){
         fetch("json/projects.json")
         .then(data=>data.json())
         .then(data=>{
-
-            let auxContent = ""
-            for(i in data.projects){
-                auxContent += 
-                `<div class="card-projects m-1">                
-                    <div class="card-projects-info">
-                    <p class="card-projects-title"><a href="${data.projects[i].link}" target="_blank">${data.projects[i].nombre}</a></p>
-                        
-                        <div class="card-projects-body">
-                            <div class="card-projects-img">
-                                <img src="assets/img/projects/${data.projects[i].img}" alt="100" width="150">
-                            </div>
-                            <div class="card-projects-text">
-                                <P><span class="value c-primary"><strong>${data.projects[i].descriptionShort}</strong></span></P>
-                                <P><span class="value"><strong>${data.projects[i].dev}</strong></span></P>
-                            </div>    
-                            <div class="card-projects-description">
-                                <p><strong>${data.projects[i].descriptionLong}.</strong></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>`
+            let auxContent = "" 
+            let nProjectsContentButtons = 0
+            let projectTotake = data.projects
+            if(data.projects.length / 3 > 1){
+                nProjectsContentButtons = parseInt((data.projects.length / 3) + 1)
+                projectsContentButtons.innerHTML = getButtonsProjectsContent(nProjectsContentButtons)
+                projectTotake = data.projects.slice(0, 3)
             }
-
+            arrayProjects = data.projects
+            auxContent = getCardProjects(projectTotake)
             projectsContent.innerHTML = auxContent
         })
         .catch(e => {
             projectsContent.innerHTML = `Ocurrió un error al cargar los proyectos, intenta recargar la página.`
         })
-
         projectsContent.innerHTML = `<div class="loader"></div>`
-        
     })
 
     //Muestra "sobre mí"
@@ -204,7 +270,8 @@ function showContent(){
       
 }
 
-window.onload = () => {
-    showContent()
-    writeWords()
-}
+
+
+
+showContent()
+writeWords()
---
+++ js/tooltips.js
@@ -0,0 +1,49 @@
+tippy('#comunicativo', {
+    content: 'Me gusta mantener una comunicación fluida con las personas con las que me relaciono, así evito cualquier tipo de malentendido',
+  });
+
+tippy('#trabajoEnEquipo', {
+    content: 'Me gusta la sensación de formar parte de algo más grande y la responsabilidad que ello conlleva',
+  });
+
+  
+tippy('#respetuoso', {
+    content: 'Soy muy profesional y educado a la hora de tratar con cualquier persona',
+  });
+
+  
+tippy('#tolerante', {
+    content: 'Soy muy tolerante con las personas, ideologías y situaciones. Lo considero un requisito obligatorio para cualquier ámbito',
+  });
+
+tippy('#responsable', {
+    content: 'Soy del tipo de persona que siente la obligación moral de cumplir con su palabra, así que cuando me comprometo a algo, lo cumplo',
+  });
+
+tippy('#puntual', {
+    content: 'Hago todo lo posible por cumplir con las fechas de entrega, respetar horarios y calendarios',
+  });
+
+tippy('#proactivo', {
+    content: 'Soy de tener la iniciativa para proponer soluciones y anticipar problemas futuros',
+  });
+
+tippy('#adaptabilidad', {
+    content: 'Me adapto fácilmente a nuevas metodologías o flujos de trabajo',
+  });
+
+tippy('#creativo', {
+    content: 'Me gusta el arte y lo que conlleva, en este campo, me gusta el arte que hay detrás de las soluciones sofisticadas a problemas aburridos',
+  });
+
+tippy('#metodico', {
+    content: 'Soy de resolver problemas siguiendo reglas predefinidas que yo mismo estipulo',
+});
+
+tippy('#paciente', {
+    content: 'Sé esperar y ser constante para obtener lo que me propongo',
+});
+
+tippy('#ordenado', {
+    content: 'Considero que la organización es el primer paso al éxito... en cualquier campo',
+});
